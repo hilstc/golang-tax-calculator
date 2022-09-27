1cfd83f8d01869a8fac31c5e51be1966a982d68d
Carla Hilst <hilstc@tcd.ie>
2022-09-27 18:03:24 -0300

    Added order.go & order_test.go

--- internal/order/entity/order_test.go
+++ internal/order/entity/order_test.go
@@ -1,1 +1,39 @@
-.
+package entity_test
+
+import (
+	"testing"
+
+	"github.com/hilstc/golang-tax-calculator/internal/order/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGivenAnEmptyId_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.T) {
+	order := entity.Order{}
+	assert.Error(t, order.IsValid(), "Invalid ID")
+}
+
+func TestGivenAnEmptyPrice_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.T) {
+	order := entity.Order{ID: "123"}
+	assert.Error(t, order.IsValid(), "Invalid price")
+}
+
+func TestGivenAnEmptyTax_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.T) {
+	order := entity.Order{ID: "123", Price: 10}
+	assert.Error(t, order.IsValid(), "Invalid tax")
+}
+
+func TestGivenValidParams_WhenCallNewOrder_ThenShould_ReceiveCreateOrderWithAllParams(t *testing.T) {
+	order, err := entity.NewOrder("123", 10, 2)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 2.0, order.Tax)
+}
+
+func TestGivenValidParams_WhenCallCalculateFinalPrice_ThenShouldCalculateFinalPriceAndSetItOnFinalPriceProperty(t *testing.T) {
+	order, err := entity.NewOrder("123", 10, 2)
+	assert.NoError(t, err)
+	err = order.CalculateFinalPrice()
+	assert.NoError(t, err)
+	assert.Equal(t, 12.0, order.FinalPrice)
+}
