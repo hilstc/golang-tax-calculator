2605814881abef129c3404b341b6d5a2cb0c1c94
Carla Hilst <hilstc@tcd.ie>
2022-09-28 18:48:32 -0300

    Updated table

--- internal/order/infra/database/order_repository.go
+++ internal/order/infra/database/order_repository.go
@@ -15,8 +15,18 @@ func NewOrderRepository(db *sql.DB) *OrderRepository {
 	return &OrderRepository{Db: db}
 }
 
+// sql create table on mysql
+// CREATE TABLE `orders` (
+//
+//	`id` varchar(255) NOT NULL,
+//	`price` float NOT NULL,
+//	`tax` float NOT NULL,
+//	`final_price` float NOT NULL,
+//	PRIMARY KEY (`id`))
+//
+// )
 func (r *OrderRepository) Save(order *entity.Order) error {
-	stmt, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_prince) VALUES (?, ?, ?, ?)")
+	stmt, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
 	if err != nil {
 		return err
 	}
