56608dcb96345d57058e6c9f28e85251e3f6880d
Carla Hilst <hilstc@tcd.ie>
2022-09-28 18:46:15 -0300

    Added channels in RabbitMQ & dashboard in Grafana

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 
 	"github.com/hilstc/golang-tax-calculator/internal/order/infra/database"
 	"github.com/hilstc/golang-tax-calculator/internal/order/usecase"
+	"github.com/hilstc/golang-tax-calculator/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -17,18 +21,52 @@ func main() {
 
 	repository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPriceUseCase(repository)
-	input := usecase.OrderInputDTO{
-		ID:    "1234",
-		Price: 100,
-		Tax:   10,
-	}
 
-	output, err := uc.Execute(input)
-
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
 	}
+	defer ch.Close()
 
-	println(output.FinalPrice)
+	out := make(chan amqp.Delivery)
 
+	go rabbitmq.Consume(ch, out)
+	go worker(out, uc, 1)
+	go worker(out, uc, 2)
+	go worker(out, uc, 3)
+
+	// input := usecase.OrderInputDTO{
+	// 	ID:    "1234",
+	// 	Price: 100,
+	// 	Tax:   10,
+	// }
+
+	// output, err := uc.Execute(input)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// println(output.FinalPrice)
+
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerId int) {
+	for msg := range deliveryMessage {
+		var input usecase.OrderInputDTO
+
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			fmt.Println("Error unmarshalling message:", err)
+		}
+
+		input.Tax = 10.0
+		_, err = uc.Execute(input)
+		if err != nil {
+			fmt.Println("Error unmarshalling message:", err)
+		}
+
+		msg.Ack(false)
+		fmt.Println("Worker", workerId, "processed order", input.ID)
+	}
 }
